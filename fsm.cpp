a3e5e1024afab904fce9c5098ef6cf33072aa7bc
Faraz Akhter <74548088+thehashmizzzaccount@users.noreply.github.com>
2023-02-19 12:27:19 +0530

    Add files via upload

---
+++ fsm.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+enum TrafficLightState {
+  RED,
+  YELLOW,
+  GREEN
+};
+
+class TrafficLight {
+public:
+  TrafficLight() : currentState(RED) {}
+
+  void buttonPressed(TrafficLightState newState) {
+    switch (newState) {
+      case RED:
+        std::cout << "Stop!" << std::endl;
+        break;
+      case YELLOW:
+        std::cout << "Prepare to stop!" << std::endl;
+        break;
+      case GREEN:
+        std::cout << "Go!" << std::endl;
+        break;
+    }
+
+    currentState = newState;
+    switch (currentState) {
+      case RED:
+        std::this_thread::sleep_for(20s); // Wait for 20 seconds
+        buttonPressed(GREEN);
+        break;
+      case YELLOW:
+        std::this_thread::sleep_for(5s); // Wait for 5 seconds
+        buttonPressed(RED);
+        break;
+      case GREEN:
+        std::this_thread::sleep_for(15s); // Wait for 15 seconds
+        buttonPressed(YELLOW);
+        break;
+    }
+  }
+
+private:
+  TrafficLightState currentState;
+};
+
+int main() {
+  TrafficLight trafficLight;
+  trafficLight.buttonPressed(RED);
+  return 0;
+}
